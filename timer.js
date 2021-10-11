ffb2b3df3f363d0b86860692aef8314b339d8580
unknown <muzzlestore@gmail.com>
2021-10-11 17:51:45 +0300

    add hw-11-3 timer

--- timer.js
+++ timer.js
@@ -1,4 +1,70 @@
 
-let date = new Date(2022, 6, 17);
+class CountdownTimer {
 
-console.log(date.getTime());
+    constructor({selector, targetDate,onTick,onLabelControl,onTimerEnd}) {
+        this.setIntervalId = null;
+        this.selector = selector;
+        this.targetDate = new Date(targetDate);
+        this.onTick = onTick;
+        this.onLabelControl = onLabelControl;
+        this.onTimerEnd = onTimerEnd;
+    }
+
+    start() {
+        this.setIntervalId = setInterval(() => {
+            const currentDate = Date.now();
+            const leftTime = this.targetDate.getTime() - currentDate;
+            if(leftTime < 0) {
+                clearInterval(this.setIntervalId);
+
+                return;
+            }
+            this.onTick(this.selector,  this.getTimeElements(leftTime));
+            this.onLabelControl(this.selector,  this.getTimeElements(leftTime));
+        ;
+        }, 1000);
+    };
+
+    pad(value){
+        return String(value).padStart(2,'0');
+    };
+
+    getTimeElements(time) {
+        const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+        const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+        return {days, hours, mins, secs};
+    };
+}
+
+function countdownRender(selector, {days, hours, mins, secs}) {
+    const timerEl = document.querySelector(`${selector}`);
+    timerEl.querySelector('span[data-value="days"]').textContent = `${days}`;
+    timerEl.querySelector('span[data-value="hours"]').textContent = `${hours}`;
+    timerEl.querySelector('span[data-value="mins"]').textContent = `${mins}`;
+    timerEl.querySelector('span[data-value="secs"]').textContent = `${secs}`;
+}
+
+function countdownLabelRender(selector, {days, hours, mins, secs}) {
+    const timerEl = document.querySelector(`${selector}`);
+    timerEl.querySelector('span[data-label="days"]').textContent = days == 1? 'Day':'Days';
+    timerEl.querySelector('span[data-label="hours"]').textContent = hours == 1? 'Hour':'Hours';
+    timerEl.querySelector('span[data-label="mins"]').textContent = mins == 1? 'Minute':'Minutes';
+    timerEl.querySelector('span[data-label="secs"]').textContent = secs == 1? 'Second':'Seconds';
+}
+
+function countdownRender(){
+                // this.digitFieldRef.forEach(element => element.classList.add('done'));
+}
+
+const timer = new CountdownTimer({
+    selector: '#timer-1',
+    targetDate: new Date(2021,9,12,18,38,40),
+    onTick: countdownRender,
+    onLabelControl: countdownLabelRender,
+    onTimerEnd:countdownRender,
+    // targetDate: new Date('Oct 12, 2024'),
+  });
+
+timer.start();
